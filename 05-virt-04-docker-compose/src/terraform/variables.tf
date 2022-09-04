8cffa81549ee868b28d0dbac7e3d19c2e0fdc7b3
Yuri Artemiev <yur-artemev@yandex.ru>
2022-09-04 19:41:56 +0300

    Added src folder

---
+++ 05-virt-04-docker-compose/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gjd8gta6ntpckrp97r"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1gcthk9ak11bmpnbo7d"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8a2pfcnlee71u1g12c"
+}
